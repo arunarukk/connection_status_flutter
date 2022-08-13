828660c3666a09fb0094a46c62c1d5cec1af557a
Arun Kumar KK <arunkumarkk158@gmail.com>
2022-08-13 17:56:14 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,197 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeService();
+  runApp(const MyApp());
+}
+
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      onStart: onStart,
+      autoStart: true,
+      isForegroundMode: true,
+    ),
+    iosConfiguration: IosConfiguration(
+      autoStart: true,
+      onForeground: onStart,
+      onBackground: onIosBackground,
+    ),
+  );
+  service.startService();
+}
+
+bool onIosBackground(ServiceInstance service) {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  return true;
+}
+
+String connectionResult() {
+  if (_connectionStatus == ConnectivityResult.wifi) {
+    return 'Wi-Fi';
+  } else if (_connectionStatus == ConnectivityResult.mobile) {
+    return 'Mobile network';
+  }
+  return 'None';
+}
+
+void onStart(ServiceInstance service) async {
+  DartPluginRegistrant.ensureInitialized();
+
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  await preferences.setString("hello", "world");
+
+  if (service is AndroidServiceInstance) {
+    service.on('setAsForeground').listen((event) {
+      service.setAsForegroundService();
+    });
+
+    service.on('setAsBackground').listen((event) {
+      service.setAsBackgroundService();
+    });
+  }
+
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+
+  Timer.periodic(const Duration(seconds: 1), (timer) async {
+    if (service is AndroidServiceInstance) {
+      initConnectivity();
+      _connectivitySubscription =
+          _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+      String message = connectionResult();
+      service.setForegroundNotificationInfo(
+        title: "Network Connection",
+        content: "You are connected to $message",
+      );
+    }
+
+    final deviceInfo = DeviceInfoPlugin();
+    String? device;
+    if (Platform.isAndroid) {
+      final androidInfo = await deviceInfo.androidInfo;
+      device = androidInfo.model;
+    }
+
+    if (Platform.isIOS) {
+      final iosInfo = await deviceInfo.iosInfo;
+      device = iosInfo.model;
+    }
+
+    service.invoke(
+      'update',
+      {
+        "current_date": DateTime.now().toIso8601String(),
+        "device": device,
+      },
+    );
+  });
+}
+
+ConnectivityResult _connectionStatus = ConnectivityResult.none;
+final Connectivity _connectivity = Connectivity();
+late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+Future<void> initConnectivity() async {
+  late ConnectivityResult result;
+  try {
+    result = await _connectivity.checkConnectivity();
+  } on PlatformException catch (e) {
+    debugPrint('Couldn\'t check connectivity status :$e');
+    return;
+  }
+
+  return _updateConnectionStatus(result);
+}
+
+Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+  _connectionStatus = result;
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String text = "Stop Service";
+
+  @override
+  void initState() {
+    initConnectivity();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Network Status'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              StreamBuilder<Map<String, dynamic>?>(
+                stream: FlutterBackgroundService().on('update'),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  String message = connectionResult();
+                  return Column(
+                    children: [
+                      const Text('Connection Status'),
+                      Text(message),
+                    ],
+                  );
+                },
+              ),
+              ElevatedButton(
+                child: Text(text),
+                onPressed: () async {
+                  final service = FlutterBackgroundService();
+                  var isRunning = await service.isRunning();
+                  if (isRunning) {
+                    service.invoke("stopService");
+                  } else {
+                    service.startService();
+                  }
+
+                  if (!isRunning) {
+                    text = 'Stop Service';
+                  } else {
+                    text = 'Start Service';
+                  }
+                  setState(() {});
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
